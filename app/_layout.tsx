cfcb36524ac38e89bd2b588111f2d30447ba2da2
Henrique <guihenrilira642@gmail.com>
2025-03-27 20:02:35 -0300

    modal adjustment on app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+      <Stack.Screen name="modal" options={{ headerShown: false }} />
     </Stack>
   );
 }
